4f6789afc6742075e4c9da4cf6420631bf7ae7c0
punitDT <punit@dharmatech.in>
2022-08-18 15:23:52 +0530

    word counter

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -10,14 +10,23 @@ function TextForm(props) {
         console.log("uppercase was clicked");
         setText(text.toUpperCase())
     }
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState("");
     return (
         <>
+        <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary" onClick={handleUpClick}>Covert to Uppercase</button>
+        </div>
+        <div className='container my-3'>
+            <h2>Your text summary</h2>
+            <p>{text.trim().split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
         </>
     )
 }
